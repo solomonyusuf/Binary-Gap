a06499fd97aa7985045c04b510dffe6c9b1b168e
olafire <olafire03@gmail.com>
2022-08-28 04:56:22 +0100

    Add project files.

---
+++ Binary Gap/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+int val = 9;
+solution(val);
+
+int solution(int N)
+{
+    var value = 0;
+    var result = new List<int>();
+    var num = Convert.ToString(N, 2);
+
+    foreach (var n in num)
+    {
+        if (n.Equals("0"))
+        {
+            var a = value++;
+            value = 0;
+        }
+        else
+        {
+            result.Add(value);
+        }
+    }
+
+    return result.Max();
+}
